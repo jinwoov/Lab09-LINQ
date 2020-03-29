d923a80bf753aa3c8e6286c63d9f7ffadf475586
Jin Kim <jinwoov@gmail.com>
2020-03-28 22:20:30 -0700

    got the json to work

---
+++ Lab09-Manhattan/Lab09-Manhattan/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Lab09_Manhattan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UsingJSON();
+        }
+
+
+        public static void UsingJSON()
+        {
+            using (StreamReader reader = File.OpenText(@"../../../data.json"))
+            {
+                JObject manhattan = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
+
+                foreach (var item in manhattan)
+                {
+                    Console.WriteLine(item);
+                }
+
+                var neighbor = manh
+
+
+            }
+        }
+    }
+}
