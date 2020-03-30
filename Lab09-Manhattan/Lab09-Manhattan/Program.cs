11791bb8081dc90289f48d0743e168d3230a8e4e
Jin Kim <jinwoov@gmail.com>
2020-03-29 22:51:37 -0700

    Created outline

--- Lab09-Manhattan/Lab09-Manhattan/Program.cs
+++ Lab09-Manhattan/Lab09-Manhattan/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Lab09_Manhattan.Classes;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace Lab09_Manhattan
 {
@@ -12,7 +10,19 @@ namespace Lab09_Manhattan
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(@"
+                        #=================#
+                        # New York Finest #
+                        #    by Jin       #
+                        #=================#");
+            Console.WriteLine("Press any key to proceed this application");
+            Console.ReadLine();
             UsingJSON();
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Sorry for a lot of enters, please enjoy rest of your day");
+            Console.ReadLine();
+            Environment.Exit(0);
+
         }
 
 
@@ -20,8 +30,6 @@ namespace Lab09_Manhattan
         {
             using (StreamReader reader = File.OpenText(@"../../../data.json"))
             {
-
-            
                 var par = reader.ReadToEnd();
 
                 var parseData = JsonConvert.DeserializeObject<ListYork>(par);
@@ -34,58 +42,70 @@ namespace Lab09_Manhattan
 
                     Console.WriteLine($"{counts}: {item.Properties.Neighborhood}");
                 }
-                Console.WriteLine();
+                Console.WriteLine("-------------------");
+                Console.WriteLine("Enter to see the neighborhoods without empty entry");
+                Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("");
 
                 var newCities =
                 from p in parseData.NewYork
                 where p.Properties.Neighborhood != ""
                 select p.Properties.Neighborhood;
 
-                Console.WriteLine("This is after trimming down null neighbors");
                 int count = 0;
                 foreach (var item in newCities)
                 {
                     count++;
                     Console.WriteLine($"{count}: {item}");
                 }
-                Console.WriteLine();
-
+                Console.WriteLine("-------------------");
+                Console.WriteLine("Enter to see the neighborhoods without duplicates");
+                Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("");
+                
                 var newCitiess = newCities.Distinct();
-                Console.WriteLine("This is after neighborhood is cleaned out of duplicate");
+                count = 0;
                 foreach (var item in newCitiess)
                 {
                     count++;
                     Console.WriteLine($"{count}: {item}");
                 }
-                Console.WriteLine();
+
+                Console.WriteLine("-------------------");
+                Console.WriteLine("Enter to see the neighborhoods sorted out efficiently using one line technique");
+                Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("");
 
                 var newerCity = parseData.NewYork
                     .Where(neighbor => neighbor.Properties.Neighborhood != "")
                     .Select(neigbor => neigbor.Properties.Neighborhood)
                     .Distinct();
 
-                Console.WriteLine("This is one liner");
                 int counter = 0;
                 foreach (var item in newerCity)
                 {
                     counter++;
                     Console.WriteLine($"{counter}: {item}");
                 }
-                Console.WriteLine();
+                Console.WriteLine("-------------------");
+                Console.WriteLine("Enter to see the second question written in one liner");
+                Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("");
 
                 var newNeighbor = parseData.NewYork
                     .Where(p => p.Properties.Neighborhood != "")
                     .Select(p => p.Properties.Neighborhood);
 
-                Console.WriteLine("Changeing the second question to one liner");
                 count = 0;
                 foreach (var item in newNeighbor)
                 {
                     count++;
                     Console.WriteLine($"{count}: {item}");
                 }
-
-
             }
         }
     }
